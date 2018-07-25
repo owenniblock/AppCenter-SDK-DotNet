1c6c2f556df1fd03b0882c062f5af0a545738531
Guillaume Perrot <guperrot@microsoft.com>
2018-07-25 14:37:40 -0700

    Update demo

--- Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: AssemblyInformationalVersion("1.8.0")]
+[assembly: AssemblyFileVersion("1.8.0.1")]
+[assembly: AssemblyInformationalVersion("1.8.0-r0001-a80612c")]
